b7982b6e3852b6cc8ada685bfd342237c8c2ec84
CaceresLucas <caceresely10@gmail.com>
2026-04-29 14:24:17 -0300

    feat: create main application layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
+import MainLayout from './layouts/MainLayout'
+
 function App() {
-  return (
-    <div>
-      <h1>MoneyBoard</h1>
-    </div>
-  )
+  return <MainLayout />
 }
 
 export default App
---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,15 @@
+function MainLayout() {
+  return (
+    <div>
+      <header>
+        <h1>MoneyBoard</h1>
+      </header>
+
+      <main>
+        <p>Financial dashboard under construction.</p>
+      </main>
+    </div>
+  )
+}
+
+export default MainLayout
