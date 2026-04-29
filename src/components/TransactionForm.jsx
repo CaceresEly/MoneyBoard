0babdc70a8f5a4f4d9fbe108ceea2dbb1cd52d10
CaceresLucas <caceresely10@gmail.com>
2026-04-29 14:51:00 -0300

    feat: implement controlled transaction form

--- src/components/TransactionForm.jsx
+++ src/components/TransactionForm.jsx
@@ -1,13 +1,46 @@
+import { useState } from 'react'
+
 function TransactionForm() {
+  const [description, setDescription] = useState('')
+  const [amount, setAmount] = useState('')
+  const [type, setType] = useState('')
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    const newTransaction = {
+      description,
+      amount: Number(amount),
+      type,
+    }
+
+    console.log(newTransaction)
+
+    // limpar campos
+    setDescription('')
+    setAmount('')
+    setType('')
+  }
+
   return (
-    <form className="transaction-form">
+    <form className="transaction-form" onSubmit={handleSubmit}>
       <h3>Add transaction</h3>
 
-      <input type="text" placeholder="Description" />
+      <input
+        type="text"
+        placeholder="Description"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
 
-      <input type="number" placeholder="Amount" />
+      <input
+        type="number"
+        placeholder="Amount"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+      />
 
-      <select defaultValue="">
+      <select value={type} onChange={(e) => setType(e.target.value)}>
         <option value="" disabled>
           Select type
         </option>
